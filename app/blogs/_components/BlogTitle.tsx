355401bc7b1f8c4dc18896a495fe4b1665490e59
Khan Atik Faisal <khanatik1176@gmail.com>
2025-06-29 15:12:23 +0600

    Remove-redundantfiles

--- app/blogs/_components/BlogTitle.tsx
+++
@@ -1,7 +0,0 @@
-const BlogTitle = ({ title }: { title: string }) => (
-    <div className='flex w-full items-center'>
-      <h1 className='w-full max-w-[800px] text-[32px] lg:text-[42px] font-semibold'>{title}</h1>
-    </div>
-  );
-  
-  export default BlogTitle;
