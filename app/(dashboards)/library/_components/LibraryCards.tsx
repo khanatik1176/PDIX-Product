8e062d97e75d60b10288091f50a028d51608870e
Khan Atik Faisal <khanatik1176@gmail.com>
2025-09-23 14:14:45 +0600

    fixed-color

--- app/(dashboards)/library/_components/LibraryCards.tsx
+++ app/(dashboards)/library/_components/LibraryCards.tsx
@@ -1,10 +1,10 @@
-import { LibraryCardProps } from "@/types/Library.types";
-import React from "react";
+import { LibraryCardProps } from '@/types/Library.types';
+import React from 'react';
 
 const LibraryCard: React.FC<LibraryCardProps> = ({ title, value }) => (
-  <div className="bg-[#FEFAEC] border rounded-lg shadow-md p-4 flex flex-col items-center mt-1">
-    <span className="text-lg font-semibold text-secondary">{title}</span>
-    <span className="text-2xl font-bold mt-2 text-primary">{value}</span>
+  <div className='mt-1 flex flex-col items-center rounded-lg border border-[#FBF0CA] bg-white p-4 shadow-md'>
+    <span className='text-lg font-semibold text-gray-500'>{title}</span>
+    <span className='mt-2 text-2xl font-bold text-black'>{value}</span>
   </div>
 );
 
